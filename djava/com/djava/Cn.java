febffd2bd8aa1161a9ea2f87c809f7d6277af09a
Dxh7788 <anan1215>
2018-05-23 15:52:34 +0800

    #3 Javac的Tree结构

---
+++ djava/com/djava/Cn.java
@@ -0,0 +1,26 @@
+package com.djava;
+
+import java.io.File;
+
+/**
+ * Copyright (C) 2017-2018 https://www.htouhui.com - A project by javac
+ *
+ * @author xh.d
+ * @since 2018/5/23 10:29
+ */
+public class Cn {
+
+    public static void main(String[] args) {
+        File pf = new File(System.getProperty("user.dir") + "\\src\\com\\sun\\source\\tree");
+        if (pf.exists()){
+            File[] files = pf.listFiles();
+            for (File file:files){
+                String name = file.getName();
+                String subName = name.substring(0,name.length()-9);
+                if (name.endsWith("Tree.java")){
+                    System.out.println(name+"<->JC"+subName);
+                }
+            }
+        }
+    }
+}
