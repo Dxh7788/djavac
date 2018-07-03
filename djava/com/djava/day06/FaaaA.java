fae63fea542ad4991218c147c2fe50802ae11761
Dxh7788 <anan1215>
2018-07-03 14:38:03 +0800

    #1 day06 CMS并发方案

--- djava/com/djava/day06/FaaaA.java
+++ djava/com/djava/day06/FaaaA.java
@@ -13,19 +13,21 @@ import java.util.Set;
 public class FaaaA {
 
     public static void main(String[] args) {
-        Map<String,String> map = null;
-        Map<String,String> copyMap = map;
-        if (copyMap == null){
-            map = new HashMap<String,String>();
-            copyMap = map;
+        //CMS方案使用只读和只写指向统一内存空间,模仿生产者和消费者
+        //readMap 只读,writeMap只写
+        Map<String,String> readMap = null;
+        Map<String,String> writeMap = readMap;
+        if (writeMap == null){
+            readMap = new HashMap<String,String>();
+            writeMap = readMap;
         }
-        copyMap.put("1","dxh");
-        copyMap.put("2","ddh");
-        if (map==null || map.isEmpty()){
+        writeMap.put("1","dxh");
+        writeMap.put("2","ddh");
+        if (readMap==null || readMap.isEmpty()){
             System.out.println("validate fail!!!");
         }else {
-            System.out.println("validate success!!!Map size is "+map.size());
-            Set<Map.Entry<String,String>> entries = copyMap.entrySet();
+            System.out.println("validate success!!!Map size is "+readMap.size());
+            Set<Map.Entry<String,String>> entries = writeMap.entrySet();
             for (Map.Entry<String,String> entry:entries){
                 System.out.println("[key:"+entry.getKey()+",value:"+entry.getValue()+"]");
             }
